b8c14802d91335b8a2878adda19893cbd109a6c1
Fernando Balderas Guzmán <fernando.bguzman@gmail.com>
2021-08-21 23:49:56 +0300

    Add useForm and useEffect

--- src/features/search/SearchForm.js
+++ src/features/search/SearchForm.js
@@ -1,40 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux'
 import { updateMovies } from './searchSlice'
 import axios from '../../helpers/axios';
+import { useForm } from "react-hook-form";
 import { TMBD_URL, API_KEY } from '../../helpers/constants';
 
 function SearchForm() {
 
-    const [query, setQuery] = useState('');
-    const [previousQuery, setPreviousQuery] = useState('');
     const dispatch = useDispatch();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [query, setQuery] = useState('');
 
-    const searchMovies = (e) => {
-        e.preventDefault();
-        if (query === '') {
+    useEffect(() => {
+        const fetchMovies = () => {
+            if (query === '') return;
+            const url = `${TMBD_URL}&api_key=${API_KEY}&query=${query}`;
+            axios.get(url)
+                .then(function(response) {
+                    dispatch(updateMovies(response.data.results));
+                });
+        }
+        fetchMovies();
+    }, [dispatch, query])
+    
+    const onSubmitSearch = (data) => {
+        const queryTrimmed = data.query.trim();
+        if (queryTrimmed === '') {
             console.warn('Empty query');
             return;
         }
-        if (query === previousQuery) {
+        if (queryTrimmed === query) {
             console.warn('Equal query');
             return;
         }
-        const url = `${TMBD_URL}&api_key=${API_KEY}&query=${query}`;
-        axios.get(url)
-            .then(function(response) {
-                dispatch(updateMovies(response.data.results));
-            });
-        setPreviousQuery(query);
+        setQuery(queryTrimmed);
     }
 
     return (
-        <form className="form" onSubmit={searchMovies}>
+        <form className="form" onSubmit={handleSubmit(onSubmitSearch)}>
             <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query" 
-                placeholder="i.e. Jurassic World"
-                value={query} onChange={(e) => setQuery(e.target.value)}
-            />
+            <div className="wrapper--input">
+                <input className="input" type="text"
+                    placeholder="i.e. Jurassic World"
+                    {...register("query", { required: true })}
+                />
+                {errors.query && <span className="alert-error">This field is required</span>}
+                </div>
             <button className="button" type="submit">Search</button>
         </form>
     )
